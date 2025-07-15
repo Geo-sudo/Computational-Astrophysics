3746a46b125f166cbdb7be1b7d763a41a8dff47c
Geo-sudo <george.nady.contact@gmail.com>
2025-07-15 18:30:32 +0300

    Round nd Round nd Round!

--- Gravitational_Potential/main.cpp
+++ Gravitational_Potential/main.cpp
@@ -1,6 +1,22 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <vector>
+#include <cmath>
+
+std::vector<float> genCircle(float center_X, float center_Y, float radius, int segments){
+    std::vector<float> vertices;
+    vertices.push_back(center_X);
+    vertices.push_back(center_Y);
+    vertices.push_back(0.0f);
+    for(int i=0; i <= segments; i++){
+        float angle = 2.0f * M_PI * float(i)/float(segments);
+        vertices.push_back(center_X + (radius * cosf(angle))); 
+        vertices.push_back(center_Y + (radius * sinf(angle)));
+        vertices.push_back(0.0f);
+    }
+    return vertices;
+}
 
 
 int main() {
@@ -10,7 +26,7 @@ int main() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "First", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(600, 600, "Gravitational_Potential", NULL, NULL);
     if(window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
@@ -19,13 +35,60 @@ int main() {
     }
     glfwMakeContextCurrent(window);
 
-    glViewport(0,0,800,600);
+    if (glewInit() != GLEW_OK) {
+    std::cout << "Failed to initialize GLEW" << std::endl;
+    return -1;
+    }
+
+    std::vector<float> vertices = genCircle(.0f,.0f,.5f,100);
+
+    unsigned int VBO, VAO;
+    glGenBuffers(1, &VBO);
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), vertices.data(), GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    const char* vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main() {\n"
+    "   gl_Position = vec4(aPos, 1.0);\n"
+    "}";
+
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    const char* fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main() {\n"
+    "   FragColor = vec4(.1, 0.8, 0.2, 1.0);\n"
+    "}";
+
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
 
     while(!glfwWindowShouldClose(window))
     {
-        glClearColor(.1f,.8f,.1f,1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
-        
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLE_FAN, 0, vertices.size()/3);
+
         glfwPollEvents();
         glfwSwapBuffers(window);
     }
